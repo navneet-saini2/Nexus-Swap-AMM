4dda45c3cc7a2f8dbbae1cc27c658efa9ac96321
navneet <navneet201204@gmail.com>
2025-12-29 21:21:08 +0530

    Add minimal ERC20 interface

--- src/SimpleDEX.sol
+++ src/SimpleDEX.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.26;
 // Nexus-Swap-AMM
 // Built from scratch
 
-contract SimpleDex {
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
 
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
+
+contract SimpleDex {}
